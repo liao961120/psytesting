30731f1626c8114f453f6a8b4cfcb184ee87f84f
Y.-F. Liao <liaomovie2@gmail.com>
2018-02-21 00:34:48 +0800

    update

--- rscript/plotting_descri.R
+++ rscript/plotting_descri.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(scales) # for ggplot color
 library(plotly)
 
-psy_test <- read_rds("./data/psy_test_analy.rds")
+psy_test_f <- read_rds("./data/psy_test_filtered.rds")
 var_info <- read_rds("./data/item_construct_conNum.rds")
 
 
@@ -19,7 +19,7 @@ blank_theme <- theme_minimal()+
         plot.title=element_text(size=14, face="bold")
     )
 
-gender <- psy_test %>%
+gender <- psy_test_f %>%
     count(gender)
     # mutate(gender= fct_recode(gender,
     #     "女"="female",
@@ -34,8 +34,8 @@ pie <- ggplot(gender, aes(x="", y=n, fill=gender))+
     coord_polar("y", start=0) +
     scale_fill_manual(values=c(hue[1], hue[3], "black"))+
     blank_theme+
-    geom_text(aes(y = n/3  + c(135, 20, 311),
-              label = percent(n/312)), size=3, vjust="outward",hjust="outward")+
-    labs(title="Gender Percentage", fill="Gender")
+    geom_text(aes(y = c(140, 43, 218), # label position adjustment
+              label = percent(n/sum(n))), size=3, vjust="outward",hjust="outward")+
+    labs(title="Gender Percentage", fill="Gender", subtitle =paste(" Samples: ",sum(gender$n), sep=""))
 
 
